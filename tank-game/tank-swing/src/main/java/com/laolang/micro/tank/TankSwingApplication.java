f369e644661538e02000184f910dc64e6c61eb21
潼关路边的一只野鬼 <xiaodaima2016@163.com>
2024-10-05 14:52:19 +0800

    🎉 init(tank-swing): 初始化项目

---
+++ tank-game/tank-swing/src/main/java/com/laolang/micro/tank/TankSwingApplication.java
@@ -0,0 +1,10 @@
+package com.laolang.micro.tank;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TankSwingApplication {
+    public static void main(String[] args) {
+        log.info("tank-swing is running...");
+    }
+}
