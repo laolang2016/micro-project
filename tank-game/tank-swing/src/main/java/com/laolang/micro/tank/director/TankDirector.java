f1701d2e6e637835a5cfc6e425f1d273195c6a11
潼关路边的一只野鬼 <xiaodaima2016@163.com>
2024-10-05 15:56:42 +0800

    ✨ feat(tank-swing): 加入素材资源

--- tank-game/tank-swing/src/main/java/com/laolang/micro/tank/director/TankDirector.java
+++ tank-game/tank-swing/src/main/java/com/laolang/micro/tank/director/TankDirector.java
@@ -13,15 +13,10 @@ public class TankDirector extends JFrame {
     }
 
     private void init() {
-        setSize(800, 600);
+        setSize(768, 720);
         setResizable(false);
         setTitle("坦克大战-swig 版");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    @Override
-    public void show() {
-        super.show();
     }
 }
